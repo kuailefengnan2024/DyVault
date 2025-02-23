4102d8039743fd444116db65a96a5569e8056ded
kuailefengnan2024 <han552401231@gmail.com>
2025-02-23 23:14:11 +0800

    '自动提交'

--- LearnPytorch/read_data.py
+++ LearnPytorch/read_data.py
@@ -1,8 +1,24 @@
 
 from torch.utils.data import Dataset
+
 help(Dataset)
 
+foo = 6
+a = 1
+
 from PIL import Image
-img_path = r"LearnPytorch\dataset\val\ants\800px-Meat_eater_ant_qeen_excavating_hole.jpg"
+img_path = r"D:\BaiduSyncdisk\DyVault\LearnPytorch\dataset\val\ants\800px-Meat_eater_ant_qeen_excavating_hole.jpg"
 img = Image.open(img_path)
 print(img.size)
+
+b = img.size
+# img.show()
+
+import os # win下文件管理的库
+img_path_dict = os.listdir(r"D:\BaiduSyncdisk\DyVault\LearnPytorch\dataset\val\ants")
+specific_img_path = img_path_dict[3]
+
+
+
+
+
