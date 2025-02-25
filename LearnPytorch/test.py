af01cf0eeb29db5e6a4ae20a9d80f92b87f2abd9
kuailefengnan2024 <han552401231@gmail.com>
2025-02-25 17:31:10 +0800

    '自动提交'

--- LearnPytorch/test.py
+++ LearnPytorch/test.py
@@ -1,13 +1,26 @@
-import torch
+# import torch
 
-dir_data = dir(torch.utils.data)
-dir_data = dir(torch.utils.data.Dataset)
-dir_data = dir(torch.cuda)
-dir_data = dir(torch.cuda.is_available())
-help(torch.cuda.is_available)
-print(dir_data)
+# dir_data = dir(torch.utils.data)
+# dir_data = dir(torch.utils.data.Dataset)
+# dir_data = dir(torch.cuda)
+# dir_data = dir(torch.cuda.is_available())
+# help(torch.cuda.is_available)
+# print(dir_data)
 
+import numpy as np
+import cv2
 
+# NumPy 示例
+array = np.array([[1, 2], [3, 4]])
+print("NumPy 数组：\n", array)
+print("数组转置：\n", array.T)
+
+# OpenCV 示例
+img = np.zeros((300, 300, 3), dtype=np.uint8)  # 创建黑色图像
+cv2.rectangle(img, (50, 50), (250, 250), (0, 255, 0), 2)  # 画绿色矩形
+cv2.imshow('Demo', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
