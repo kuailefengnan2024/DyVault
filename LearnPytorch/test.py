3c64e6f5497302db8cce30811ac8f81c9aa0a9b2
kuailefengnan2024 <han552401231@gmail.com>
2025-04-06 22:57:43 +0800

    '自动提交'

--- LearnPytorch/test.py
+++ LearnPytorch/test.py
@@ -1,27 +1,44 @@
-# import torch
+from torchvision import transforms
+from PIL import Image
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
-# dir_data = dir(torch.utils.data)
-# dir_data = dir(torch.utils.data.Dataset)
-# dir_data = dir(torch.cuda)
-# dir_data = dir(torch.cuda.is_available())
-# help(torch.cuda.is_available)
-# print(dir_data)
+# 加载图片
+image = Image.open(r'LearnPytorch\dataset\train\ants\5650366_e22b7e1065.jpg')
 
-import numpy as np
-import cv2
+# 使用 transforms.ToTensor() 转换图像为张量
+transform = transforms.ToTensor()
+image_tensor = transform(image)
 
-# NumPy 示例
-array2 = np.array([[1, 2], [3, 4]])
-print("NumPy 数组：\n", array)
-print("数组转置：\n", array.T)
+# 打印原始张量的形状 (C, H, W)
+print("原始图像张量形状:", image_tensor.shape)
 
-# OpenCV 示例
-img = np.zeros((300, 300, 3), dtype=np.uint8)  # 创建黑色图像
-cv2.rectangle(img, (50, 50), (250, 250), (0, 255, 0), 2)  # 画绿色矩形
-cv2.imshow('Demo', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# 添加批次维度，因为池化函数需要 (N, C, H, W) 格式
+image_tensor_batch = image_tensor.unsqueeze(0)
 
+# 应用最大池化 (kernel_size=2, stride=2)
+pooled_tensor = F.max_pool2d(image_tensor_batch, kernel_size=8, stride=8)
 
+# 移除批次维度以便显示
+pooled_tensor = pooled_tensor.squeeze(0)
 
+print("池化后图像张量形状:", pooled_tensor.shape)
 
+# 显示原始图像和池化后的图像
+plt.figure(figsize=(10, 5))
+
+# 显示原始图像
+plt.subplot(1, 2, 1)
+plt.imshow(image_tensor.permute(1, 2, 0))  # 转换为 (H, W, C) 格式用于显示
+plt.title("原始图像")
+plt.axis('off')
+
+# 显示池化后的图像
+plt.subplot(1, 2, 2)
+plt.imshow(pooled_tensor.permute(1, 2, 0))  # 转换为 (H, W, C) 格式用于显示
+plt.title("池化后图像")
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
